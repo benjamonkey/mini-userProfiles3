9420c19554b49a610ec92e3ac4428dfe258566aa
Ben Hutchings <benhutchings@Devs-MacBook-Pro.local>
2016-05-26 15:43:46 -0600

    lots of angular

--- service.js
+++ service.js
@@ -1,10 +1,20 @@
-angular.module('userProfiles').service('mainService', function($http) {
+angular.module('userProfiles').service('mainService', function($http, $q) {
 
   this.getUsers = function() {
+    var deferred = $q.defer();
     return $http({
         method: 'GET',
         url: 'http://reqres.in/api/users?page=1'
+    })then(function(response){
+      var parsedResponse = resonse.data.data;
+      for (var i = 0; i < parsedResponse.length; i++){
+        parsedResponse[i].first_name = 'Ralf';
+      }
+
+      deffered.resolve(response)
     })
+    return deffered.promise;
+
   }
 
 });
